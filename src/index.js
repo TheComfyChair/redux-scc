43cf6935c5efa7d8782d96b7d15cb2a6fd5fc913
Kai Moseley <kai.moseley@onfido.com>
2016-12-09 13:52:43 +0000

    Remove longer example

--- src/index.js
+++ src/index.js
@@ -35,38 +35,11 @@ const exampleReducer = {
     })
 };
 
-const exampleReducer2 = {
-    screen: Types.reducer(Types.shape({
-        handlerIsCBS: Types.boolean(true),
-        handlerIsENG: Types.boolean(true),
-        invoices: Types.arrayOf(Types.string()),
-        activeInvoice: Types.number(-1),
-        deleteInvoiceModalActive: Types.boolean(),
-        suppInvoiceModalActive: Types.boolean(),
-        paymentModalActive: Types.boolean(),
-        resetModalActive: Types.boolean(),
-        reinstateModalActive: Types.boolean(),
-    })),
-    delete: Types.reducer(Types.shape({
-        invoiceSequence: Types.number(),
-        vehicleSequence: Types.number(),
-        deleteReason: Types.string(),
-    })),
-    supp: Types.reducer(Types.shape({
-        invoiceSequence: Types.number(),
-        vehicleSequence: Types.number(),
-        mode: Types.string(),
-        amount: Types.string(),
-    })),
-};
-
 const test = buildReducer('example', exampleReducer);
-const test2 = buildReducer('invoices', exampleReducer2);
 
 const store = createStore(
     combineReducers({
         ...test.reducers,
-        ...test2.reducers,
     }),
     compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
 );
@@ -80,8 +53,6 @@ console.log(111, test.selectors.example.form2(store.getState()));
 console.log(222, test.actions);
 console.log(333, test.selectors);
 console.log(444, test.selectors.example.primitiveTest(store.getState()));
-console.log(555, test2.selectors, test2.actions);
-console.log(666, test2.selectors.screen(store.getState()));
 
 store.dispatch(test.actions.example.arrayTest.replace([1,2,3]));
 store.dispatch(test.actions.example.arrayTest.updateAtIndex(5, 0));
